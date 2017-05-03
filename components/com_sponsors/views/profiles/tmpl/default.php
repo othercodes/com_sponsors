c41e6e897ea14d484144b1af49ac10fa96851a63
Unay Santisteban <davidu@softcom.com>
2017-05-03 14:37:57 +0200

    Added support to 1140 banner

--- components/com_sponsors/views/profiles/tmpl/default.php
+++ components/com_sponsors/views/profiles/tmpl/default.php
@@ -17,11 +17,13 @@ $counter = 1;
         <div class="banner-patrocinador">
             <?php if ($this->params->get('details') == 1): ?><a href="<?php echo JRoute::_('index.php?option=com_sponsors&view=profile&id=' . (int)$item->id); ?>"><?php endif ?>
                 <?php if ($this->params->get('banner') == 1): ?>
-                    <img src="<?php echo $item->banner1; ?>" alt="<?php echo $this->escape($item->name); ?>"/>
-                <?php else: ?>
-                    <img src="<?php echo $item->banner2; ?>" alt="<?php echo $this->escape($item->name); ?>"/>
+                    <img src="<?php echo $item->banner1; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:468px;"/>
+                <?php elseif($this->params->get('banner') == 2): ?>
+                    <img src="<?php echo $item->banner2; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:150px;"/>
+                <?php else:?>
+                    <img src="<?php echo $item->banner3; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:1140px;"/>
                 <?php endif ?>
-                <?php if ($this->params->get('details') == 1): ?></a><?php endif ?>
+            <?php if ($this->params->get('details') == 1): ?></a><?php endif ?>
         </div>
     </div>
     <?php if ($this->params->get('columns') == $counter++) : ?>
