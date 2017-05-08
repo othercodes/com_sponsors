b3e3edf8f758389278a8539e81752e24b2fba2d9
Unay Santisteban <usantisteban@othercode.es>
2017-05-08 20:40:18 +0200

    Added web link to non-vip sponsors

--- components/com_sponsors/views/profiles/tmpl/default.php
+++ components/com_sponsors/views/profiles/tmpl/default.php
@@ -9,7 +9,7 @@
 defined('_JEXEC') or die;
 
 $counter = 1;
-if($this->params->get('random', 0) == 1) {
+if ($this->params->get('random', 0) == 1) {
     shuffle($this->items);
 }
 ?>
@@ -18,15 +18,23 @@ if($this->params->get('random', 0) == 1) {
     <?php foreach ($this->items as $i => $item) : ?>
     <div class="span<?php echo round(12 / $this->params->get('columns')); ?>">
         <div class="banner-patrocinador">
-            <?php if ($this->params->get('details') == 1 && $item->vip == 1): ?><a href="<?php echo JRoute::_('index.php?option=com_sponsors&view=profile&id=' . (int)$item->id); ?>"><?php endif ?>
-                <?php if ($this->params->get('banner') == 1): ?>
-                    <img src="<?php echo $item->banner1; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:468px;"/>
-                <?php elseif($this->params->get('banner') == 2): ?>
-                    <img src="<?php echo $item->banner2; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:150px;"/>
-                <?php else:?>
-                    <img src="<?php echo $item->banner3; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:1140px;"/>
-                <?php endif; ?>
-            <?php if ($this->params->get('details') == 1 && $item->vip == 1): ?></a><?php endif ?>
+            <?php if ($item->vip == 1) : ?>
+
+            <a href="<?php echo JRoute::_('index.php?option=com_sponsors&view=profile&id=' . (int)$item->id); ?>">
+                <?php else : ?>
+
+                <a href="<?php echo $item->url; ?>" rel="nofollow" target="_blank">
+
+                    <?php endif; ?>
+
+                    <?php if ($this->params->get('banner') == 1): ?>
+                        <img src="<?php echo $item->banner1; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:468px;"/>
+                    <?php elseif ($this->params->get('banner') == 2): ?>
+                        <img src="<?php echo $item->banner2; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:150px;"/>
+                    <?php else: ?>
+                        <img src="<?php echo $item->banner3; ?>" alt="<?php echo $this->escape($item->name); ?>" style="max-width:1140px;"/>
+                    <?php endif; ?>
+                </a>
         </div>
     </div>
     <?php if ($this->params->get('columns') == $counter++) : ?>
